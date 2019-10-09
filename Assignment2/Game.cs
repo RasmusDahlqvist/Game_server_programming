0814b726bd80ee1744f0967ba7ba965533315b0b
dahlqvisti <dahlqvistrasmus@gmail.com>
2019-10-10 00:40:39 +0300

    Add files via upload

---
+++ Assignment2/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Assignment2
+{
+    public class Game<T> where T : IPlayer
+    {
+        private List<T> _players;
+
+        public Game(List<T> players)
+        {
+            _players = players;
+        }
+
+        public T[] GetTop10Players()
+        {
+            // ... write code that returns 10 players with highest scores
+
+            return _players.Take(10).ToArray();
+        }
+    }
+}
